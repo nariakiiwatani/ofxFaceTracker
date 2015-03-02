5dc55e279156849088b387327d580cd1eed52ce4
nariakiiwatani <nariakiiwatani@annolab.com>
2015-03-03 01:17:24 +0900

    added multi ofxFaceTrackerMulti

---
+++ src/ofxFaceTrackerMulti.h
@@ -0,0 +1,38 @@
+#pragma once
+
+#include "ofxCv.h"
+using namespace ofxCv;
+
+template<typename Tracker>
+class ofxFaceTrackerMulti
+{
+public:
+	void setup(int maximum=1);
+	bool update(cv::Mat image);
+	vector<Tracker*>& getTrackers() { return trackers_; }
+private:
+	vector<Tracker*> trackers_;
+};
+
+template<typename Tracker>
+void ofxFaceTrackerMulti<Tracker>::setup(int maximum)
+{
+	trackers_.resize(maximum);
+	for(int i = 0; i < maximum; ++i) {
+		trackers_[i] = new Tracker();
+		trackers_[i]->setup();
+	}
+}
+template<typename Tracker>
+bool ofxFaceTrackerMulti<Tracker>::update(cv::Mat image)
+{
+	for(Tracker *tracker : trackers_) {
+		if(tracker->update(image)) {
+			cv::circle(image, toCv(tracker->getPosition()), tracker->getScale()*25, cv::Scalar(0,0,0), -1);
+		}
+		else {
+			break;
+		}
+	}
+}
+
