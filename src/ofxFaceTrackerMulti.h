b55dc03bafaa8f10d04178b3edb42908897964ac
nariakiiwatani <nariakiiwatani@annolab.com>
2015-03-14 22:30:32 +0900

    small changes

--- src/ofxFaceTrackerMulti.h
+++ src/ofxFaceTrackerMulti.h
@@ -10,8 +10,10 @@ public:
 	void setup(int maximum=1);
 	bool update(cv::Mat image);
 	vector<Tracker*>& getTrackers() { return trackers_; }
+	int getDiscardSize() { return discard_size_; }
 private:
 	vector<Tracker*> trackers_;
+	int discard_size_;
 };
 
 template<typename Tracker>
@@ -22,13 +24,17 @@ void ofxFaceTrackerMulti<Tracker>::setup(int maximum)
 		trackers_[i] = new Tracker();
 		trackers_[i]->setup();
 	}
+	discard_size_ = 25;
 }
 template<typename Tracker>
 bool ofxFaceTrackerMulti<Tracker>::update(cv::Mat image)
 {
 	for(Tracker *tracker : trackers_) {
 		if(tracker->update(image)) {
-			cv::circle(image, toCv(tracker->getPosition()), tracker->getScale()*25, cv::Scalar(0,0,0), -1);
+			cv::circle(image, toCv(tracker->getPosition()), tracker->getScale()*discard_size_, cv::Scalar(0,0,0), -1);
+		}
+		else {
+			image.setTo(cv::Scalar(0,0,0));
 		}
 	}
 }
